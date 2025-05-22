1e2acfbbe99123988d9a2c1da951045eb8545dc0
Thomas Johannesson <jordfras@gmail.com>
2025-05-22 21:11:36 +0200

    Initial unit tests

--- src/lib.rs
+++ src/lib.rs
@@ -70,3 +70,63 @@ fn mock_name(class: &model::ClassToMock<'_>) -> String {
         class.class.get_name().expect("Class should have a name")
     )
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::sync::Mutex;
+
+    macro_rules! lines {
+        () => {
+            String::new()
+        };
+        ($line:literal $( $rest:literal)*) => {
+            format!("{}\n{}", $line, lines!($($rest)*))
+        };
+    }
+
+    macro_rules! assert_mocks {
+        ($actual_mocks:expr $(, $expected_mock:expr)*) => {
+            let expected_mocks = vec![$(($expected_mock)),*];
+            assert_eq!($actual_mocks, expected_mocks);
+        };
+    }
+
+    // Mutex needs to be locked in beginning of each test to avoid running in parallel.
+    // clang::Clang can only be used from one thread at a time.
+    static IN_SERIAL: Mutex<()> = Mutex::new(());
+
+    #[test]
+    fn simple_pure_virtual_function_can_be_mocked() {
+        let _guard = IN_SERIAL.lock().unwrap();
+        let mocksmith = MockSmith::new();
+        let cpp_class = "
+          class Foo {
+          public:
+            virtual ~Foo() = default;
+            virtual void bar() = 0;
+          };";
+        assert_mocks!(
+            mocksmith.create_mocks_from_string(cpp_class),
+            lines!(
+                "class MockFoo : public Foo"
+                "{"
+                "public:"
+                "  MOCK_METHOD(void, bar, (), (override));"
+                "};"
+            )
+        );
+    }
+
+    #[test]
+    fn simple_non_virtual_function_is_ignored() {
+        let _guard = IN_SERIAL.lock().unwrap();
+        let mocksmith = MockSmith::new();
+        let cpp_class = "
+          class Foo {
+          public:
+            void bar();
+          };";
+        assert!(mocksmith.create_mocks_from_string(cpp_class).is_empty());
+    }
+}
