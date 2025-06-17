b876dd47fad50d6df8a8794a91a595657b241947
Thomas Johannesson <jordfras@gmail.com>
2025-06-17 19:19:49 +0200

    Introduce test helper functions for generic cpp class/mock
    - Also fix command line argument in test
    - Also rename functions to `expect` prefix in program_under_test mod

--- tests/app_integration_tests.rs
+++ tests/app_integration_tests.rs
@@ -6,45 +6,44 @@ mod program_under_test;
 
 use program_under_test::Mocksmith;
 
-#[test]
-fn input_from_stdin_can_be_mocked() {
-    let mut mocksmith = Mocksmith::run(&[]);
-    mocksmith.write_stdin(&lines!(
-        "class ISomething {"
-        "public:"
-        "  virtual void fun() = 0;"
+// Creates class to mock, when not really interested in the actual content.
+fn some_class(name: &str) -> String {
+    lines!(
+        format!("class {} {{", name),
+        "public:",
+        "  virtual void fun() = 0;",
         "};"
-    ));
+    )
+}
+
+// Creates a mock for a class produced by `some_class()`
+fn some_mock(class_name: &str, mock_name: &str) -> String {
+    lines!(
+        format!("class {} : public {}", mock_name, class_name),
+        "{",
+        "public:",
+        "  MOCK_METHOD(void, fun, (), (override));",
+        "};"
+    )
+}
+
+#[test]
+fn input_from_stdin_produces_mock_only() {
+    let mut mocksmith = Mocksmith::run(&[]);
+    mocksmith.write_stdin(&some_class("ISomething"));
     mocksmith.close_stdin();
 
-    assert_ok!(mocksmith.read_stdout(&lines!(
-        "class MockSomething : public ISomething"
-        "{"
-        "public:"
-        "  MOCK_METHOD(void, fun, (), (override));"
-        "};\n"
-    )));
+    assert_ok!(mocksmith.expect_stdout(&some_mock("ISomething", "MockSomething")));
 
     assert!(mocksmith.wait().success());
 }
 
 #[test]
 fn mocks_can_be_named_with_sed_style_regex() {
-    let mut mocksmith = Mocksmith::run(&[r"--name=s/I(.*)/Fake\1/"]);
-    mocksmith.write_stdin(&lines!(
-        "class ISomething {"
-        "public:"
-        "  virtual void fun() = 0;"
-        "};"
-    ));
+    let mut mocksmith = Mocksmith::run(&[r"--name-mock=s/I(.*)/Fake\1/"]);
+    mocksmith.write_stdin(&some_class("ISomething"));
     mocksmith.close_stdin();
 
-    assert_ok!(mocksmith.read_stdout(&lines!(
-        "class FakeSomething : public ISomething"
-        "{"
-        "public:"
-        "  MOCK_METHOD(void, fun, (), (override));"
-        "};\n"
-    )));
+    assert_ok!(mocksmith.expect_stdout(&some_mock("ISomething", "FakeSomething")));
     assert!(mocksmith.wait().success());
 }
